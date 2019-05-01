af18d8c43cbc864337918ab18fb0edf72ff059a7
David Goldstein <david.goldstein@deciphernow.com>
2019-05-01 14:36:26 -0400

    midnight pr commit 2, I solved it?git add -A

--- problem_10.c
+++ problem_10.c
@@ -5,8 +5,13 @@
  * @return {char} is prime
  **/
 char isPrime(unsigned int number) {
-	if (number % 2 == 0) return 1;
-    return 0;
+	// taken from https://forgetcode.com/c/386-prime-number-using-function
+	int c;
+	for ( c = 2 ; c <= number - 1 ; c++ ) { 
+	   if ( number%c == 0 )
+	  return 0;
+	}
+	return 1;
 }
 
 int main() {
