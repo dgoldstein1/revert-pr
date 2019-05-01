3c9977cd21601a09e51f3d8843d81f3303ba6b0e
David Goldstein <david.goldstein@deciphernow.com>
2019-05-01 14:12:42 -0400

    wrote out main function

--- problem_10.c
+++ problem_10.c
@@ -5,9 +5,16 @@
  * @return {char} is prime
  **/
 char isPrime(unsigned int number) {
-    return 1;
+    return 0;
 }
 
 int main() {
+	long acc = 1;
+	for (int i = 1 ; i < 2000000 ; i += 2) {
+		if (isPrime(i)) acc+=i;
+	}
+
+	printf("%li \n", acc);
+
 	return 0;
 }
